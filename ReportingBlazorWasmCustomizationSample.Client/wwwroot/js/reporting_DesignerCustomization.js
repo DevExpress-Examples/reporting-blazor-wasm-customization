e722a54072e1713da1e9b47b5e3c8b6722993079
Polina Tyureva <polina.tyureva@devexpress.com>
2025-01-07 12:56:41 +0400

    update example

--- ReportingBlazorWasmCustomizationSample.Client/wwwroot/js/reporting_DesignerCustomization.js
+++ ReportingBlazorWasmCustomizationSample.Client/wwwroot/js/reporting_DesignerCustomization.js
@@ -1,7 +1,7 @@
 ﻿window.ReportingDesignerCustomization = {
     onCustomizeElements: function(s, e) {
         //Remove Menu button
-        var menuButton = e.GetById(s.dx.Reporting.Designer.Utils.ReportDesignerElements.MenuButton)
+        var menuButton = e.GetById(DevExpress.Reporting.Designer.Utils.ReportDesignerElements.MenuButton)
         var menuButtonIndex = e.Elements.indexOf(menuButton);
         menuButtonIndex !== -1 && e.Elements.splice(menuButtonIndex, 1);
     },
@@ -11,13 +11,13 @@
         firstToolbarItem.hasSeparator = true;
 
         //Move New button to the toolbar
-        var newReportAction = e.GetById(s.dx.Reporting.Designer.Actions.ActionId.NewReport);
+        var newReportAction = e.GetById(DevExpress.Reporting.Designer.Actions.ActionId.NewReport);
         newReportAction.container = "toolbar";
         e.Actions.splice(e.Actions.indexOf(newReportAction), 1);
         e.Actions.splice(0, 0, newReportAction);
 
         //Move Save button to the toolbar
-        var saveAction = e.GetById(s.dx.Reporting.Designer.Actions.ActionId.Save);
+        var saveAction = e.GetById(DevExpress.Reporting.Designer.Actions.ActionId.Save);
         saveAction.container = "toolbar";
         e.Actions.splice(e.Actions.indexOf(saveAction), 1);
         e.Actions.splice(0, 0, saveAction);
@@ -30,7 +30,7 @@
     onCustomizeWizard: function(s, e) {
         if(e.Type === "ReportWizard") {
             e.Wizard.events.addHandler("beforePageInitialize", (args) => {
-                if (args.pageId === s.dx.Reporting.Designer.Wizard.FullscreenReportWizardPageId.SelectReportTypePage) {
+                if (args.pageId === DevExpress.Reporting.Designer.Wizard.FullscreenReportWizardPageId.SelectReportTypePage) {
                     args.page.typeItems.splice(0, 1);
                     args.page.typeItems.pop();
                 }
